8025e63f55188df31383cc14998ae325429b0c2f
karate <tsimpidisd@gmail.com>
2019-05-04 00:12:30 +0300

    Fix indentation

--- app/classes/Reader.php
+++ app/classes/Reader.php
@@ -3,22 +3,22 @@
 namespace Gallery;
 
 class Reader {
-	private static $images = [];
+  private static $images = [];
 
    /**
     * Read the 'gallery' directory and get all .jpg files
     */
-	static function read_gallery(): void {
-		echo "Reading gallery...\n";
-		$directory = Settings::get_image_source_dir();
-		$images = glob($directory . "*");
-		foreach ($images as &$image) {
-			$image = str_replace($directory, '', $image);
-		}
-		self::$images = $images;
-	}
+  static function read_gallery(): void {
+    echo "Reading gallery...\n";
+    $directory = Settings::get_image_source_dir();
+    $images = glob($directory . "*");
+    foreach ($images as &$image) {
+      $image = str_replace($directory, '', $image);
+    }
+    self::$images = $images;
+  }
 
-	static function get_images(): array {
-		return self::$images;
-	}
+  static function get_images(): array {
+    return self::$images;
+  }
 }
--- app/classes/Settings.php
+++ app/classes/Settings.php
@@ -2,16 +2,16 @@
 namespace Gallery;
 
 use Symfony\Component\Yaml\Yaml;
-	
+
 class Settings {
-	private static $settings;
+  private static $settings;
 
   /**
    * Parse the app/settings.yml file
    */
-	static function read(): void {
-		echo "Reading settings...\n";
-		$settings = Yaml::parse(file_get_contents('app/settings.yml'));
+  static function read(): void {
+    echo "Reading settings...\n";
+    $settings = Yaml::parse(file_get_contents('app/settings.yml'));
 
     // Add trailing slash
     foreach ($settings['paths'] as &$path) {
@@ -19,22 +19,22 @@ class Settings {
     }
 
     self::$settings = $settings;
-	}
+  }
 
   /**
    * Getters FTW!
    */
-	static function get_all(): array {
-		return self::$settings;
-	}
+  static function get_all(): array {
+    return self::$settings;
+  }
 
-	static function get_site_name(): string {
-		return self::$settings['site']['name'];
-	}
+  static function get_site_name(): string {
+    return self::$settings['site']['name'];
+  }
 
-	static function get_site_description(): string {
-		return self::$settings['site']['description'];
-	}
+  static function get_site_description(): string {
+    return self::$settings['site']['description'];
+  }
 
   static function get_image_width(): int {
     return self::$settings['site']['images']['max_width'];
@@ -44,9 +44,9 @@ class Settings {
     return self::$settings['site']['images']['thumb_width'];
   }
 
-	static function get_image_source_dir(): string {
-		return self::$settings['paths']['image_source_dir'];
-	}
+  static function get_image_source_dir(): string {
+    return self::$settings['paths']['image_source_dir'];
+  }
 
   static function get_export_dir(): string {
     return self::$settings['paths']['export_dir'];
